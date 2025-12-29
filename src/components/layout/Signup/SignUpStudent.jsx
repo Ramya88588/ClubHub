bcc48bcc8dae1e5c78c1cd96038b3ee80285a2e6
Leruva <sjruva2006@gmail.com>
2025-12-29 22:25:06 +0530

    feat(student-settings): enable editing profile info section

--- src/components/layout/Signup/SignUpStudent.jsx
+++ src/components/layout/Signup/SignUpStudent.jsx
@@ -2,7 +2,8 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { auth } from "@/firebase/firebase";
-import { createStudentProfile } from "@/firebase/collections";
+import { createStudent } from "@/firebase/collections";
+import { createUser } from "@/firebase/collections"
 
 const SignUpStudent = () => {
   const navigate = useNavigate();
@@ -20,24 +21,58 @@ const SignUpStudent = () => {
     return null;
   }
 
+  // const onSubmit = async (data) => {
+  //   try {
+  //     const phoneNumber = `${data.countryCode}${data.phone}`;
+
+  //     await createStudentProfile(user.uid, {
+  //       name: data.fullName,
+  //       phoneNumber,
+  //       email: user.email,
+  //     });
+
+  //     // Redirect to student dashboard
+  //     navigate("/student");
+  //   } catch (error) {
+  //     console.error("Student signup failed:", error);
+  //     alert("Something went wrong. Please try again.");
+  //   }
+  // };
+
   const onSubmit = async (data) => {
     try {
-      const phoneNumber = `${data.countryCode}${data.phone}`;
+      const user = auth.currentUser;
 
-      await createStudentProfile(user.uid, {
-        name: data.fullName,
-        phoneNumber,
+      if (!user) {
+        alert("Not authenticated");
+        return;
+      }
+
+      await createUser(user.uid, {
         email: user.email,
+        role: "STUDENT",
+        isApproved: true,
+        isActive: true,
+        fullName: data.fullName,
+        phone: `${data.countryCode}${data.phone}`,
+        createdAt: new Date(),
       });
 
-      // Redirect to student dashboard
+      
+      // await createStudent(user.uid, { ... })
+      await createStudent(user.uid, {
+        fullName: data.fullName,
+        email: user.email,
+        phone: `${data.countryCode}${data.phone}`,
+        photoURL: user.photoURL,
+      });
       navigate("/student");
-    } catch (error) {
-      console.error("Student signup failed:", error);
-      alert("Something went wrong. Please try again.");
+    } catch (err) {
+      console.error("Signup failed:", err);
+      alert("Signup failed");
     }
-  };
-  
+  };  
+
   return (
     <div className="bg-[#f8f9fa] min-h-screen flex items-center px-4 justify-center">
       <div className="bg-white border rounded-xl max-w-md w-full mx-auto py-6 px-10 flex flex-col gap-6">
